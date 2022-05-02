6600260f3b55e20564ace09f84387e8b1299db07
Harrison Daniel <hd4n@outlook.com>
2022-05-01 23:36:10 -0400

    README update

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,7 +32,9 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -41,6 +43,24 @@ function generateMarkdown(data) {
   # ${data.title}
 
   # ${data.description}
+
+  # ${table of contents}
+
+  # ${Installation}
+
+  # ${Usage}
+
+  # ${License}
+
+  # ${Contributing}
+
+  # ${Tests}
+
+  # ${Questions}
+
+
+
+
 
 `;
 }
