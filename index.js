e28907c7532b2c6bc60898f292b887e81c856e34
Harrison Daniel <hd4n@outlook.com>
2022-05-01 22:20:45 -0400

    added licensing options for prompt

--- index.js
+++ index.js
@@ -5,55 +5,123 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 // const questions = [];
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'What is the project description?'
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the installation instructions for your project?'
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'What is the usage for your project?'
-    },
-    {
-      type: 'input',
-      name: 'contribution',
-      message: 'What are the contribution guidelines to your project?'
-    },
-    {
-      type: 'input',
-      name: 'test',
-      message: 'What test instructions does your project have?'
-    },
-    {
-      type: '',
-      name: 'license',
-      message: 'Please select the license used for this project'
-    },
-    {
-      type: 'input',
-      name: 'username',
-      message: 'What is your Github username for this project?'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?'
-    }
-  ])
-  .then(answers => console.log(answers));
+const promptUser = () => {
+
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'What is the title of your project? (Requried)',
+          validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('Please enter a title for your project!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'What is the project description?',
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('Please enter a description for your project!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'installation',
+          message: 'What are the installation instructions for your project?',
+          validate: installationInput => {
+            if (installationInput) {
+              return true;
+            } else {
+              console.log('Please enter the installation instructions for your project!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'usage',
+          message: 'What is the usage for your project?',
+          validate: usageInput => {
+            if (usageInput) {
+              return true;
+            } else {
+              console.log('Please enter the usage for your project!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'contribution',
+          message: 'What are the contribution guidelines to your project?',
+          validate: contributionInput => {
+            if (contributionInput) {
+              return true;
+            } else {
+              console.log('Please enter the contribution guidelines for your project!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'test',
+          message: 'What test instructions does your project have?',
+          validate: testInput => {
+            if (testInput) {
+              return true;
+            } else {
+              console.log('Please enter your project test instruction!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'license',
+          message: 'Please choose all licenses that apply for this project',
+          choices: ['MIT', 'Apache 2.0', 'ISC', 'BSD', 'GPLv2', 'GPLv3', 'AGPLv3', 'None']
+        },
+        {
+          type: 'input',
+          name: 'username',
+          message: 'What is your Github username?',
+          validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub username!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'What is your email address?',
+          validate: emailInput => {
+            if (emailInput) {
+              return true;
+            } else {
+              console.log('Please enter your email address!');
+              return false;
+            }
+          }
+        }
+      ])
+      .then(answers => console.log(answers));
+
+  }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
